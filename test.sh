7a63e7f048dc9c512fc4073dd6776cb5a507ddb6
krzsztfwtk <kw306765@student.polsl.pl>
2023-12-23 10:26:21 +0100

    updated testing bash script

--- test.sh
+++ test.sh
@@ -4,19 +4,19 @@
 make -C encryption/
 
 # Encrypt the input file
-./encryption/encryption -i resources/frankenstein.txt -o resources/frankenstein.enc -k "supersecret"
+./encryption/encryption -i resources/frankenstein.txt -o resources/frankenstein.enc -k supersecret
 
 # Clean up the encryption module
 make -C encryption/ clean
 
-# Build decryption module
-make -C decryption/
+# Build breaker module
+make -C breaker/
 
-# Decrypt the encrypted file
-./decryption/vigenere -i resources/frankenstein.enc -w resources/hamlet.txt -k resources/frankenstein.key -o resources/frankenstein.dec
+# Breake cipher and decrypt the encrypted file
+./breaker/breaker -i resources/frankenstein.enc -w resources/hamlet.txt -k resources/frankenstein.key -o resources/frankenstein.dec
 
-# Clean up the decryption module
-make -C decryption/ clean
+# Clean up the breaker module
+make -C breaker/ clean
 
 # Display the result
 echo "If everything works correctly there should have been created files /resources/frankenstein.enc frankenstein.key (here should be \"supersecret\") frankenstein.dec (here should be text exactly the same like in frankenstein.txt but without non-alpha chars)"
